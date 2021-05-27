af6576f2d5b2358a7c155f061c2da0c3ebef978a
HiuberMorente <hiuberr@gmail.com>
2021-05-27 16:41:35 -0600

    Normalidad

--- Views/modules/empleados.php
+++ Views/modules/empleados.php
@@ -5,7 +5,7 @@
       <div class="row mb-2">
         <div class="col-sm-6">
           <h1>
-            Administrar empleados 1
+            Administrar empleados
           </h1>
         </div>
         <div class="col-sm-6">
